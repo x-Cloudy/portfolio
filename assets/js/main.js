da6383e640c45010ef2b3e222b37b0225131c105
Juan Rodrigues <cloudibr@gmail.com>
2023-04-27 20:52:55 -0300

    responsividade para mobile adicionada

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,18 @@
         const sizeCanvas = Canvas.getPropertyValue('width');
         console.log(sizeCanvas)
 
-        if(sizeCanvas < 600) {
+        if (sizeCanvas < 600) {
             console.log('OIA EU AQUI')
         }
     })();
-    
-    
-    
-    
+
     (function eClick() {
         document.addEventListener('click', (e) => {
             const event = e.target;
             console.log(event)
-            
+
             const homeAside = document.querySelector
-            ('#home-aside');
+                ('#home-aside');
             homeAside.getAttributeNames()
             if (event.classList.contains('btn-home')) {
                 homeAside.classList.add('asideShow');
@@ -28,6 +25,15 @@
                 homeAside.classList.remove('asideShow')
             }
         });
+
+        (function bloquearRolagem() {
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+            window.onscroll = function () {
+                window.scrollTo(scrollLeft, scrollTop);
+            };
+        })();
 
     })();
 
