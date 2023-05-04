10a060c2cec8a9a3f543c73f60fbfb6c1f595438
Juan Rodrigues <cloudibr@gmail.com>
2023-05-04 19:26:34 -0300

    responsividade do botão projetos add

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,8 @@
             const barraEsquerda = document.querySelector('.barra-esquerda');
             const linhaDiagonal1 = document.querySelector('#linha-diagonal1');
             const linhaDiagonal2 = document.querySelector('#linha-diagonal2');
+            const textoNome = document.querySelector('#nome-text');
+            const imgMenu = document.querySelector('.menu-project');
 
             //HOME BUTTON
             if (event.classList.contains('btn-home')) {
@@ -21,21 +23,33 @@
 
             //PROJECTS BUTTON
             if (event.classList.contains('btn-projects')) {
-                header.style.transform = "translate(-80px, 0px)"
+                header.style.transform = "translate(-50px, 0px)"
                 barraEsquerda.classList.add('barra-esquerdaClicked');
                 quadradoBarra.classList.add('quadrado-barraClicked')
+                
+                //IMG ANIMATION
+                imgMenu.style.opacity = "1";
+                imgMenu.style.transition = "opacity 2s ease-in-out";
+                
+                //TextoAnimation
+                textoNome.style.opacity = "0";
                 
                 //RESET DELAY
                 linhaDiagonal1.style.opacity = "0";
                 linhaDiagonal1.style.transitionDelay = "0s";
                 linhaDiagonal2.style.opacity = "0";
                 linhaDiagonal2.style.transitionDelay = "0s";
-            
             } else {
                 header.style.transform = "translate(0px, 0px)";
                 barraEsquerda.classList.remove('barra-esquerdaClicked');
                 quadradoBarra.classList.remove('quadrado-barraClicked')
-            
+                
+                imgMenu.style.opacity = "0";
+                imgMenu.style.transition = "0s";
+                
+                //TextoAnimation
+                textoNome.style.opacity = "1";
+                
                 //COMEBACK DELAY
                 linhaDiagonal1.style.opacity = "1";
                 linhaDiagonal1.style.transitionDelay = "1s";
@@ -43,10 +57,10 @@
                 linhaDiagonal2.style.transitionDelay = "1s";
             }
         });
-        
+        //PREVENT SCROLL
         (function bloquearRolagem() {
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
             window.onscroll = function () {
                 window.scrollTo(scrollLeft, scrollTop);
