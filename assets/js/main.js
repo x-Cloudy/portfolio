0d8ebd1a158c9dc2229fc796fa3fa03846221a83
Juan Rodrigues <cloudibr@gmail.com>
2023-05-18 15:18:57 -0300

    estrutura do info fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@
     (function eClick() {
         document.addEventListener('click', (e) => {
             const event = e.target;
-            console.log(event)
 
             const header = document.querySelector('header');
             const homeAside = document.querySelector('#home-aside');
@@ -15,14 +14,15 @@
             const imgMenu = document.querySelector('.menu-project');
             const info = document.querySelector('.info');
 
+            
             //HOME BUTTON
             if (event.classList.contains('btn-home')) {
                 homeAside.classList.add('asideShow');
             } else if (
                 event.classList.contains('particles-js-canvas-el') ||
-                              event.classList.contains('btn-info') ||
-                              event.classList.contains('btn-projects')
-                ){  
+                event.classList.contains('btn-info') ||
+                event.classList.contains('btn-projects')
+            ) {
                 homeAside.classList.remove('asideShow')
             }
 
@@ -31,34 +31,34 @@
                 header.style.transform = "translate(-50px, 0px)"
                 barraEsquerda.classList.add('barra-esquerdaClicked');
                 quadradoBarra.classList.add('quadrado-barraClicked')
-                
+
                 //IMG ANIMATION
                 imgMenu.style.opacity = "1";
                 imgMenu.style.transition = "opacity 2s ease-in-out";
-                
+
                 //TextoAnimation
                 textoNome.style.opacity = "0";
-                
+
                 //RESET DELAY
                 linhaDiagonal1.style.opacity = "0";
                 linhaDiagonal1.style.transitionDelay = "0s";
                 linhaDiagonal2.style.opacity = "0";
                 linhaDiagonal2.style.transitionDelay = "0s";
             } else if (
-                      event.classList.contains('particles-js-canvas-el') ||
-                                    event.classList.contains('btn-home') ||
-                                    event.classList.contains('btn-info')
-            ){
+                event.classList.contains('particles-js-canvas-el') ||
+                event.classList.contains('btn-home') ||
+                event.classList.contains('btn-info')
+            ) {
                 header.style.transform = "translate(0px, 0px)";
                 barraEsquerda.classList.remove('barra-esquerdaClicked');
                 quadradoBarra.classList.remove('quadrado-barraClicked')
-                
+
                 imgMenu.style.opacity = "0";
                 imgMenu.style.transition = "0s";
-                
+
                 //TextoAnimation
                 textoNome.style.opacity = "1";
-                
+
                 //COMEBACK DELAY
                 linhaDiagonal1.style.opacity = "1";
                 linhaDiagonal1.style.transitionDelay = "1s";
@@ -67,21 +67,21 @@
             }
 
             //INFO BUTTON
-            if(event.classList.contains('btn-info')) {
+            if (event.classList.contains('btn-info')) {
                 info.classList.add('infoClicked');
             } else if (
                 event.classList.contains('particles-js-canvas-el') ||
                 event.classList.contains('btn-home') ||
                 event.classList.contains('btn-projects') ||
                 event.classList.contains('menu-project') ||
-                event.classList.contains('img-project') 
+                event.classList.contains('img-project')
 
-                ) {
+            ) {
                 info.classList.remove('infoClicked');
             }
 
             //COPY EMAIL
-            if(event.classList.contains('email')) {
+            if (event.classList.contains('email')) {
                 let input = document.createElement('input');
                 input.value = "@gmail.com";
                 document.body.appendChild(input);
@@ -92,30 +92,27 @@
                 const btnEmail = document.querySelector('.email');
                 btnEmail.innerText = "Copy";
                 btnEmail.style.color = "#5a5a5a"
-                
+
                 setTimeout(function () {
                     btnEmail.innerText = "E-mail ↗";
                     btnEmail.style.color = "#e9e6e6"
                 }, 800)
             }
         });
-        //PREVENT SCROLL
-        (function bloquearRolagem() {
-            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-
-            window.onscroll = function () {
-                window.scrollTo(scrollLeft, scrollTop);
-            };
-        })();
+       
 
     })();
 
 
+     //PREVENT SCROLL
+     (function bloquearRolagem() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
-
-
-
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+    })();
 
 
 })();
